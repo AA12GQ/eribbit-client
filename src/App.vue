ec2574672cd17a6ea0f21f9f2bd1a32533b86fed
AA12GQ <875151567@qq.com>
2022-03-09 14:53:27 +0800

    请求响应拦截器

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    App
+  <div class="container">
+    <!-- 修改数据，测试是否持久化 -->
+    App {{$store.state.user.profile.account}}
+    <button @click="$store.commit('user/setUser',{account:'王炸'})">设置用户信息</button>
   </div>
 </template>
 
