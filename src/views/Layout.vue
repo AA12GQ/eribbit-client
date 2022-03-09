05ded630c17b7f65a0de84032ab97ae1e9b790cc
AA12GQ <875151567@qq.com>
2022-03-09 20:17:09 +0800

    首页-底部布局

--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -1,23 +1,25 @@
 <template>
   <AppTopnav/>
   <AppHeader/>
-  <nav>公共顶栏</nav>
-  <header>公共头部</header>
   <main>
     <!-- 二级路由 -->
     <RouterView></RouterView>
   </main>
-  <footer>公共底部</footer>
+  <AppFooter/>
 </template>
 
 <script>
 import AppTopnav from '@/components/app-topnav.vue'
 import AppHeader from '@/components/app-header.vue'
+import AppFooter from '@/components/app-footer.vue'
 export default {
   name: 'xtx-layout',
-  components: { AppTopnav, AppHeader }
+  components: { AppTopnav, AppHeader, AppFooter }
 }
 </script>
 
 <style scoped lang="less">
+.app-body {
+  min-height: 600px;
+}
 </style>
