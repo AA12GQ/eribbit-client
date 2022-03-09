a59f898487957d4354590c6045a9ba2adcc2dcc6
AA12GQ <875151567@qq.com>
2022-03-09 16:14:47 +0800

    首页-头部布局

--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -1,5 +1,6 @@
 <template>
   <AppTopnav/>
+  <AppHeader/>
   <nav>公共顶栏</nav>
   <header>公共头部</header>
   <main>
@@ -11,9 +12,10 @@
 
 <script>
 import AppTopnav from '@/components/app-topnav.vue'
+import AppHeader from '@/components/app-header.vue'
 export default {
   name: 'xtx-layout',
-  components: { AppTopnav }
+  components: { AppTopnav, AppHeader }
 }
 </script>
 
