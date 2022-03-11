17b8d44b3e71e8f171430750bca722b7c3b1a9e4
AA12GQ <875151567@qq.com>
2022-03-11 17:18:12 +0800

    首页主体-左侧分类-弹层展示

--- src/views/home/components/home-category.vue
+++ src/views/home/components/home-category.vue
@@ -1,7 +1,7 @@
 <template>
   <div class='home-category'>
     <ul class="menu">
-      <li v-for="item in menuList" :key="item.id">
+      <li v-for="item in menuList" :key="item.id" @mouseenter="categoryId=item.id">
         <RouterLink :to="`/category/${item.id}`">{{item.name}}</RouterLink>
         <template v-if="item.children">
           <RouterLink
@@ -13,34 +13,57 @@
         </template>
       </li>
     </ul>
+    <!-- 弹层 -->
+    <div class="layer">
+      <h4>分类推荐 <small>根据您的购买或浏览记录推荐</small></h4>
+      <ul v-if="currentCategory && currentCategory.goods">
+        <li v-for="item in currentCategory.goods" :key="item.id">
+          <RouterLink to="/">
+            <img :src="item.picture" alt="">
+            <div class="info">
+              <p class="name ellipsis-2">{{item.name}}</p>
+              <p class="desc ellipsis">{{item.desc}}</p>
+              <p class="price"><i>¥</i>{{item.price}}</p>
+            </div>
+          </RouterLink>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
 import { useStore } from 'vuex'
-import { reactive, computed } from 'vue'
+import { reactive, computed, ref } from 'vue'
 export default {
   name: 'HomeCategory',
   setup () {
+    const store = useStore()
+    // 最终的数据 = 9个分类 + 1 个品牌
     const brand = reactive({
       id: 'brand',
       name: '品牌',
-      children: [{ id: 'brand-chilren', name: '品牌推荐' }]
+      children: [{ id: 'brand-children', name: '品牌推荐' }]
     })
-
-    const store = useStore()
     const menuList = computed(() => {
-      const arr = store.state.category.list.map(item => {
+      const list = store.state.category.list.map(item => {
         return {
           id: item.id,
           name: item.name,
-          children: item.children && item.children.slice(0, 2)
+          children: item.children && item.children.slice(0, 2),
+          goods: item.goods
         }
       })
-      arr.push(brand)
-      return arr
+      list.push(brand)
+      return list
     })
-    return { menuList }
+
+    // 得到弹出层的推荐商品数据
+    const categoryId = ref(null)
+    const currentCategory = computed(() => {
+      return menuList.value.find(item => item.id === categoryId.value)
+    })
+    return { menuList, categoryId, currentCategory }
   }
 }
 </script>
@@ -49,7 +72,7 @@ export default {
 .home-category {
   width: 250px;
   height: 500px;
-  background: rgba(0,0,0,0.8);
+  background: rgba(0, 0, 0, 0.8);
   position: relative;
   z-index: 99;
   .menu {
@@ -65,6 +88,79 @@ export default {
         color: #fff;
         &:first-child {
           font-size: 16px;
+        }
+      }
+    }
+  }
+  &:hover {
+    .layer {
+      display: block;
+    }
+  }
+  .layer {
+    width: 990px;
+    height: 500px;
+    background: rgba(255, 255, 255, 0.8);
+    position: absolute;
+    left: 250px;
+    top: 0;
+    display: none;
+    padding: 0 15px;
+    h4 {
+      font-size: 20px;
+      font-weight: normal;
+      line-height: 80px;
+      small {
+        font-size: 16px;
+        color: #666;
+      }
+    }
+    ul {
+      display: flex;
+      flex-wrap: wrap;
+      li {
+        width: 310px;
+        height: 120px;
+        margin-right: 15px;
+        margin-bottom: 15px;
+        border: 1px solid #eee;
+        border-radius: 4px;
+        background: #fff;
+        &:nth-child(3n) {
+          margin-right: 0;
+        }
+        a {
+          display: flex;
+          width: 100%;
+          height: 100%;
+          align-items: center;
+          padding: 10px;
+          &:hover {
+            background: #e3f9f4;
+          }
+          img {
+            width: 95px;
+            height: 95px;
+          }
+          .info {
+            padding-left: 10px;
+            line-height: 24px;
+            width: 190px;
+            .name {
+              font-size: 16px;
+              color: #666;
+            }
+            .desc {
+              color: #999;
+            }
+            .price {
+              font-size: 22px;
+              color: @priceColor;
+              i {
+                font-size: 16px;
+              }
+            }
+          }
         }
       }
     }
