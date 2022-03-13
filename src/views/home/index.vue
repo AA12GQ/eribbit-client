bcb403f83712ff28f26273b31095f71ab94df4dc
AA12GQ <875151567@qq.com>
2022-03-13 13:49:42 +0800

    首页主体-最新专题

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -13,9 +13,11 @@
       <!-- 人气推荐 -->
       <HomeHot/>
       <!-- 热门品牌 -->
-    <HomeBrand />
+      <HomeBrand />
       <!-- 商品区域 -->
-    <HomeProduct />
+      <HomeProduct />
+      <!-- 最新专题 -->
+      <HomeSpecial />
   </div>
 </template>
 
@@ -26,9 +28,18 @@ import HomeNew from './components/home-new'
 import HomeHot from './components/home-hot'
 import HomeBrand from './components/home-brand'
 import HomeProduct from './components/home-product'
+import HomeSpecial from './components/home-special'
 export default {
   name: 'PageHome',
-  components: { HomeCategory, HomeBanner, HomeNew, HomeHot, HomeBrand, HomeProduct }
+  components: {
+    HomeCategory,
+    HomeBanner,
+    HomeNew,
+    HomeHot,
+    HomeBrand,
+    HomeProduct,
+    HomeSpecial
+  }
 }
 </script>
 
