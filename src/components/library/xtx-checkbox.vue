97a4ab15aab3c78e76c75950dfc758a87b19b573
AA12GQ <875151567@qq.com>
2022-03-14 13:27:50 +0800

    二级类目-复选框组件封装

---
+++ src/components/library/xtx-checkbox.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="xtx-checkbox" @click="changeChecked">
+    <i class="iconfont" :class="modelValue ? 'icon-checked' : 'icon-unchecked'"/>
+     <span v-if="$slots.default" > <slot /> </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'XtxCheckbox',
+  props: {
+    modelValue: {
+      type: Boolean,
+      default: false
+    }
+  },
+  setup (props, { emit }) {
+    const changeChecked = () => {
+      const temp = !props.modelValue
+      emit('update:modelValue', temp)
+    }
+    return { changeChecked }
+  }
+}
+</script>
+
+<style scoped lang="less">
+.xtx-checkbox {
+  display: inline-block;
+  margin-right: 2px;
+  cursor: pointer;
+  user-select: none;
+  .icon-checked {
+    color: @xtxColor;
+    ~ span {
+      color: @xtxColor;
+    }
+  }
+  i {
+    position: relative;
+    top: 1px;
+  }
+  span {
+    margin-left: 2px;
+  }
+}
+</style>
