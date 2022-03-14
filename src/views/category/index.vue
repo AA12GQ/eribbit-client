ac86e1f43da46937813e8b7b3091b7dd809ea1e7
AA12GQ <875151567@qq.com>
2022-03-14 12:37:02 +0800

    顶级类目-分类商品

--- src/views/category/index.vue
+++ src/views/category/index.vue
@@ -1,27 +1,30 @@
 <template>
-  <div class="top-category"  v-if="subList">
+  <div class="top-category">
     <div class="container">
       <!-- 面包屑 -->
       <XtxBread>
         <XtxBreadItem to="/">首页</XtxBreadItem>
-        <XtxBreadItem>{{ subList.name }}</XtxBreadItem>
+          <transition name="fade-right" mode="out-in">
+        <XtxBreadItem :key="topCategory.id">{{topCategory.name}}</XtxBreadItem>
+         </transition>
       </XtxBread>
       <!-- 轮播图 -->
-      <XtxCarousel :sliders="sliders" style="height:500px" />
+      <XtxCarousel :sliders="sliders" style="height:500px" autoPlay="true"/>
       <!-- 所有二级分类 -->
       <div class="sub-list">
         <h3>全部分类</h3>
         <ul>
-          <li v-for="item in subList.children" :key="item.id">
-            <RouterLink :to="`/category/sub/${item.id}`">
-              <img :src="item.picture" >
-              <p>{{ item.name }}</p>
-            </RouterLink>
+          <li v-for="sub in topCategory.children" :key="sub.id">
+            <a href="javascript:;">
+              <img :src="sub.picture" >
+              <p>{{sub.name}}</p>
+            </a>
           </li>
         </ul>
       </div>
       <!-- 不同分类商品 -->
-     <div class="ref-goods" v-for="sub in subList" :key="sub.id">
+           <!-- 分类关联商品 -->
+      <div class="ref-goods" v-for="sub in subList" :key="sub.id">
         <div class="head">
           <h3>- {{sub.name}} -</h3>
           <p class="tag">温暖柔软，品质之选</p>
@@ -37,9 +40,10 @@
 <script>
 import { findBanner } from '@/api/home'
 import { findTopCategory } from '@/api/category'
-import { ref, watch } from 'vue'
+import { ref, watch, computed } from 'vue'
 import { useRoute } from 'vue-router'
 import GoodsItem from './components/goods-item.vue'
+import { useStore } from 'vuex'
 export default {
   name: 'TopCategory',
   components: { GoodsItem },
@@ -59,11 +63,23 @@ export default {
     watch(() => route.params.id, (newVal) => {
       newVal && getSubList()
     }, { immediate: true })
-
+    const store = useStore()
+    const topCategory = computed(() => {
+      let cate = {}
+      // 当前顶级分类 == 根据路由上ID去category模块中的list查找
+      // console.log(route.params.id)
+      // console.log(store.state.category.list)
+      const item = store.state.category.list.find(item => {
+        return item.id === route.params.id
+      })
+      if (item) cate = item
+      return cate
+    })
     return {
       sliders,
       subList,
-      getSubList
+      getSubList,
+      topCategory
     }
   }
 }
