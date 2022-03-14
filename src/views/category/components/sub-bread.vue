0f53ed6e25bdadfc64a16c59fc936aa768defe10
AA12GQ <875151567@qq.com>
2022-03-14 13:01:11 +0800

    二级类目-展示面包屑

---
+++ src/views/category/components/sub-bread.vue
@@ -0,0 +1,13 @@
+<template>
+  <XtxBread>
+    <XtxBreadItem to="/">首页</XtxBreadItem>
+    <XtxBreadItem to="/">一级分类</XtxBreadItem>
+    <XtxBreadItem>二级分类</XtxBreadItem>
+  </XtxBread>
+</template>
+
+<script>
+export default {
+  name: 'SubBread'
+}
+</script>
