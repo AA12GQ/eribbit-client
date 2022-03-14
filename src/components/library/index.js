37704f9cef3fa5420ab99efb7e2223b314446ad5
AA12GQ <875151567@qq.com>
2022-03-14 12:31:06 +0800

    顶级类目-分类商品

--- src/components/library/index.js
+++ src/components/library/index.js
@@ -1,50 +1,65 @@
-import XtxSkeleton from './xtx-skeleton'
-import XtxCarousel from './xtx-carousel.vue'
-import XtxMore from './xtx-more.vue'
 import defaultImg from '@/assets/images/200.png'
-import { useIntersectionObserver } from '@vueuse/core'
-import XtxBread from './xtx-bread.vue'
-import XtxBreadItem from './xtx-bread-item.vue'
+// import XtxSkeleton from './xtx-skeleton.vue'
+// import XtxCarousel from './xtx-carousel.vue'
+// import XtxMore from './xtx-more.vue'
+// import XtxBread from './xtx-bread.vue'
+// import XtxBreadItem from './xtx-bread-item.vue'
 
+// 批量注册组件
+// 导入libray文件夹下的文件
+// 批量导入组件需要使用到 require.context(dir,deep,matching)
+// 第一个参数是 目录 第二个参数是 子目录 第三个参数是 加载的正则表达式
 const importFn = require.context('./', false, /\.vue$/)
-console.dir(importFn.keys())
-
-const defineDirective = (app) => {
-  app.directive('lazy', {
-    mounted (el, { value }) {
-      const { stop } = useIntersectionObserver(
-        el,
-        // isIntersecting 布尔值，元素可见为 true，元素不可见为 false
-        ([{ isIntersecting }], observerElement) => {
-          // 图片标签是否可见
-          if (isIntersecting) {
-            // 如果目标可见，替换图片地址，自动加载图片
-            el.src = value
-            // 如果图片地址是错误的，无法加载图片，显示占位图
-            el.onerror = () => {
-              el.src = defaultImg
-            }
-            stop()
-          }
-        },
-        { threshold: [0] }
-      )
-    }
-  })
-}
 
 export default {
   install (app) {
-    app.component(XtxSkeleton.name, XtxSkeleton)
-    app.component(XtxCarousel.name, XtxCarousel)
-    app.component(XtxMore.name, XtxMore)
-    app.component(XtxBread.name, XtxBread)
-    app.component(XtxBreadItem.name, XtxBreadItem)
-    defineDirective(app)
+    // 在app上进行扩展，app提供 component directive 函数
+    // 如果要挂载原型 app.config.globalProperties 方式
+    // app.component(XtxSkeleton.name, XtxSkeleton)
+    // app.component(XtxCarousel.name, XtxCarousel)
+    // app.component(XtxMore.name, XtxMore)
+    // app.component(XtxBread.name, XtxBread)
+    // app.component(XtxBreadItem.name, XtxBreadItem)
 
-    importFn.keys.forEach(key => {
+    // 批量注册全局组件
+    importFn.keys().forEach(key => {
+      // 导入组件
       const component = importFn(key).default
+      // 注册组件
       app.component(component.name, component)
     })
+
+    // 定义指令
+    defineDirective(app)
   }
+}
+// 图片懒加载函数
+const defineDirective = (app) => {
+  // 1、图片懒加载指令 v-lazy
+  // 原理：先存储图片地址 不能在src上 ，当图片进入可视区，将你存储图片地址设置给图片元素即可
+  app.directive('lazy', {
+    // vue2.0监听dom是否创建好，钩子函数，inserted
+    // vue3.0指令拥有的钩子函数和组件一样 使用指令的dom是否创建好 使用mounted函数
+    mounted (el, binding) {
+      // 创建一个观察对象 来观察当前使用指令的对象
+      // 第一个是回调函数，第二个是配置选项
+      // isIntersecting用来判断是否进入可视区
+      const observe = new IntersectionObserver(([{ isIntersecting }]) => {
+        // 停止观察
+        if (isIntersecting) {
+          observe.unobserve(el)
+          // 把指令的值 设置给el的src属性 binding.value就是指令的值
+          // 处理图片加载失败
+          el.onerror = () => {
+            el.src = defaultImg
+          }
+          el.src = binding.value
+        }
+      }, {
+        threshold: 0 // 设置交叉比例为0
+      })
+      // 开启观察
+      observe.observe(el)
+    }
+  })
 }
