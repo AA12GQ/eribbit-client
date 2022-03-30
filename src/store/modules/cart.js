1fd31af5960c6c5e7f9bbb4d1ed7bef53c2d274a
AA12GQ <875151567@qq.com>
2022-03-30 19:47:47 +0800

    add README.md

--- src/store/modules/cart.js
+++ src/store/modules/cart.js
@@ -4,5 +4,33 @@ export default {
     return {
       list: []
     }
+  },
+  mutations: {
+    // 加入购物车
+    insertCart (state, payload) {
+      // 考虑两种情况：
+      //  情况1：已经添加过的商品(skuid已存在)，是数量的累加
+      //  情况2：没有添加过的就添加到列表前
+      const index = state.list.findIndex(item => item.skuId === payload.skuId)
+      if (index >= 0) {
+        state.list[index].count += payload.count
+      } else {
+        state.list.unshift(payload)
+      }
+    }
+  },
+  actions: {
+    insertCart (store, goods) {
+      return new Promise((resolve, reject) => {
+        if (store.rootState.user.token) {
+          // 已登录 TODO
+
+        } else {
+          // 未登录
+          store.commit('insertCart', goods)
+          resolve()
+        }
+      })
+    }
   }
 }
